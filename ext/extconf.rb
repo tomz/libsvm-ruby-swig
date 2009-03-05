48c26d5175cfa6b35051f55497cadad39d2379b9
dev <tom.z.zeng@gmail.com>
2009-03-05 11:06:06 -0500

    forces CC to g++ from the default cc

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,4 +1,5 @@
 require 'mkmf'
+CONFIG["CC"] = "g++"
 $CFLAGS = "#{ENV['CFLAGS']} -Wall -O3 "
 if CONFIG["MAJOR"].to_i >= 1 && CONFIG["MINOR"].to_i >= 8
   $CFLAGS << " -DHAVE_DEFINE_ALLOC_FUNCTION"
